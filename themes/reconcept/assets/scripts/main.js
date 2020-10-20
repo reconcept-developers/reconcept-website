df02617b1d405810dee57a512313b1fe19247060
Jürgen Visser <jurgen@reconcept.nl>
2020-10-20 12:51:22 +0200

    mobile menu, contact form styling

--- themes/reconcept/assets/scripts/main.js
+++ themes/reconcept/assets/scripts/main.js
@@ -1,8 +1,10 @@
 import $ from "jquery";
+import ContactForm from "./contact-form";
 import Navigation from "./navigation";
 import SubNavigation from "./sub-navigation";
 
 $(document).ready(function () {
   new Navigation();
   new SubNavigation();
+  new ContactForm();
 });
