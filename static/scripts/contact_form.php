5f642573c75789390ae19231c8221d786389c66e
Jürgen Visser <jurgenvisser@Jurgens-MBP.home>
2020-12-14 11:42:37 +0100

    email form fix

--- static/scripts/contact_form.php
+++ static/scripts/contact_form.php
@@ -1,6 +1,10 @@
 <?php
 require 'send_mail.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // $from_email = "jurgen@reconcept.nl";
 // $from_name = "Jurgen Visser";
 // $subject = "Hello";
@@ -15,13 +19,23 @@ $request = [];
 parse_str($postdata, $request);
 
 // Email address
-$from_email = $request['email']; // required
-$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+$from_email = 'info@reconcept.nl';
+$to_email = $request['to_email']; // required
+$to_emails = explode(",", $to_email);
+// only allow to_email if it contains @reconcept.nl
+$reconcept_email_exp = '/^[A-Za-z0-9._%-]+@reconcept.nl$/';
+// $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
 
 // check email validity
-if(!$from_email || !preg_match($email_exp, $from_email)){
-	return http_response_code(400);
+foreach ($to_emails as $key => $email) {
+  $to_emails[$key] = trim($email);
+  if(!$email || !preg_match($reconcept_email_exp, trim($email))){
+    // echo "Invalid email ".$email;
+    return http_response_code(400);
+  }
 }
+
+$to_email_joined = implode($to_emails, ",");
 
 // Email name
 $from_name = htmlspecialchars($request['name']);
@@ -42,7 +56,7 @@ function clean_string($string) {
 $message .= clean_string($post_message)."\n";
 
 // Send the mail
-$result = send_mailgun_mail($from_email, $from_name, $subject, $message);
+$result = send_mailgun_mail($to_email_joined, $from_email, $from_name, $subject, $message);
 
 //return a response code
 http_response_code($result->http_response_code);
--- static/scripts/send_mail.php
+++ static/scripts/send_mail.php
@@ -3,7 +3,7 @@
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
 
-function send_mailgun_mail($from_email, $from_name, $subject, $message){
+function send_mailgun_mail($to_email, $from_email, $from_name, $subject, $message){
 
 	# Instantiate the client.
 	$mgClient = new Mailgun('key-afdd6e06110d512825a1af2e7f544f4d');
@@ -12,7 +12,7 @@ function send_mailgun_mail($from_email, $from_name, $subject, $message){
 	# Make the call to the client.
 	$result = $mgClient->sendMessage($domain, array(
 	    'from'    => $from_name.' <'.$from_email.'>',
-		'to'      => 'Jürgen Visser <jurgen@reconcept.nl>, Reconcept <info@reconcept.nl>',
+		  'to'      => $to_email,
 	    'subject' => $subject.' - Reconcept website',
 	    'text'    => $message
 	));
