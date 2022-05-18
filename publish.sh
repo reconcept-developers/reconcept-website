dce89cd969eb1f221f7a4357183a476e354a8c09
Jürgen <jurgen@reconcept.nl>
2022-05-18 15:24:52 +0200

    alternative homepage for hogeschoools

--- publish.sh
+++ publish.sh
@@ -5,12 +5,22 @@
 #     echo "The working directory is dirty. Please commit any pending changes."
 #     exit 1;
 # fi
+CONFIG=$1
+
+CONFIG_FILENAME="config.toml"
+
+if [ "$CONFIG" = "dev" ]; then
+  CONFIG_FILENAME="config-dev.toml"
+fi
+
+echo $CONFIG
+echo $CONFIG_FILENAME
 
 echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo
+hugo --config $CONFIG_FILENAME
 
 echo "Uploading site"
-rsync -avzr -e "ssh -p 2509" --progress public/ reconcept@185.27.174.207:/www/reconcept-home/
+rsync -avzr -e "ssh -p 2509" --progress public/ reconcept@185.27.174.207:/www/reconcept-home/$CONFIG
