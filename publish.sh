3746b6dd2f1e627af828bbd638a44937d56bded9
Jürgen <jurgen@reconcept.nl>
2022-07-06 11:12:40 +0200

    update deploy script to S3

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# set error so that script will stop on error
+set -e
+
 # if [ "`git status -s`" ]
 # then
 #     echo "The working directory is dirty. Please commit any pending changes."
@@ -8,6 +11,9 @@
 CONFIG=$1
 
 CONFIG_FILENAME="config.toml"
+
+BUCKET="reconcept.nl"
+DISTRIBUTIONID="EZBWG58HOG40X"
 
 if [ "$CONFIG" = "dev" ]; then
   CONFIG_FILENAME="config-dev.toml"
@@ -22,5 +28,10 @@ rm -rf public/*
 echo "Generating site"
 hugo --config $CONFIG_FILENAME
 
-echo "Uploading site"
-rsync -avzr -e "ssh -p 2509" --progress public/ reconcept@185.27.174.207:/www/reconcept-home/$CONFIG
+echo "$BUCKET ---[Copying package to server]---"
+aws s3 sync ./public s3://$BUCKET
+
+echo "---[Creating invalidation]---"
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTIONID --paths "/*"
+
+printf "\n\n!!! DONE DEPLOYING !!!\n\n"
